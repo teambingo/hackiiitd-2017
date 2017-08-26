ed14d93c6f3393bbd55064372f82da298f2e4a4a
aniket965 <aniket965.as@gmail.com>
2017-08-26 05:58:58 +0530

    Mazor changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,42 +1,8 @@
-var recognition;  
-function startRecognition() {
-    	console.log("started");
-        recognition = new webkitSpeechRecognition();
-        recognition.onstart = function(event) {
-        	console.log("started");
-        };
-        recognition.onresult = function(event) {
-            var text = "";
-            for (var i = event.resultIndex; i < event.results.length; ++i) {
-                text += event.results[i][0].transcript;
-            }
-            console.log(text);
-            stopRecognition();
-        };
-        recognition.onend = function() {
-            stopRecognition();
-        };
-        recognition.lang = "en-US";
-        recognition.start();
-    }
 
+  
+  $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+    
+  });
 
-    /*
-     * for stop switching;
-     **/
-
-    function stopRecognition() {
-    	console.log("stoped");
-        if (recognition) {
-            recognition.stop();
-            recognition = null;
-        }
-    }
-
-    function switchRecognition() {
-        if (recognition) {
-            stopRecognition();
-        } else {
-            startRecognition();
-        }
-    }
